e6f788415152b2d94a9a1eb00a953faaeb16dc0b
LeeKann <ekannampuzha@cpp.edu>
2017-10-23 18:47:34 -0700

    Added Eric test of the jsoup mapping

---
+++ src/test/java/edu/csupomona/cs480/EricTest.java
@@ -0,0 +1,31 @@
+package edu.csupomona.cs480;
+
+import edu.csupomona.cs480.controller.WebController;
+import junit.framework.TestCase;
+
+public class EricTest extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public void testJSoup() {
+		try {
+			String expectedVal = "Target Finds: " + "\n" +"<a href=\"javascript:void(0)\" class=\"link link-grayDarkest\" rel=\"nofollow\">terms</a>\n" +
+					"<a href=\"javascript:void(0)\" class=\"link link-grayDarkest\" rel=\"nofollow\">privacy</a>\n" +
+					"<a href=\"javascript:void(0)\" class=\"link link-grayDarkest\" rel=\"nofollow\">interest-based ads</a>\n" +
+					"<a href=\"javascript:void(0)\" class=\"link link-grayDarkest\" rel=\"nofollow\">ca privacy rights</a>\n" +
+					"<a href=\"javascript:void(0)\" class=\"link link-grayDarkest\" rel=\"nofollow\">ca supply chain act</a>\n" +
+					"<a href=\"javascript:void(0)\" class=\"js-privacy-update link link-orange\" data-value=\"privacyPolicy\" data-ref=\"/spot/terms/privacy-policy\"></a>";
+			String returnVal = WebController.jsoup();
+			assertEquals(returnVal, expectedVal);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
