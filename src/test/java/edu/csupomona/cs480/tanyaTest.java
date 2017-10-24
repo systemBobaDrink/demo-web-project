8a9bb1158cffeb3547fa9d4930c552b91a76fb0a
tanyaw <twanwatanakool@gmail.com>
2017-10-23 17:28:25 -0700

    Cleaned up test file.

--- src/test/java/edu/csupomona/cs480/tanyaTest.java
+++ src/test/java/edu/csupomona/cs480/tanyaTest.java
@@ -1,30 +1,25 @@
 package edu.csupomona.cs480;
 
-import org.junit.After;
-import org.junit.Before;
-
 import edu.csupomona.cs480.controller.WebController;
-import junit.framework.Test;
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 public class tanyaTest extends TestCase {
 	private String eventName;
 
 	protected void setUp() throws Exception {
-	super.setUp();
-	String eventName = "Event1";
+		super.setUp();
+		String eventName = "Event1";
 	}
-	
+
 	protected void tearDown() throws Exception {
-	super.tearDown();
-	String eventName = "";
+		super.tearDown();
+		String eventName = "";
 	}
-	
+
 	public void testGetEvent() {
-	String expectedVal = "Event ID";
-	String returnVal = WebController.getEvent(eventName);
-	assertEquals(returnVal, expectedVal);
+		String expectedVal = "Event ID";
+		String returnVal = WebController.getEvent(eventName);
+		assertEquals(returnVal, expectedVal);
 	}
-	
+
 }
