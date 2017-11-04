49c6a878c49c82b514b916db9d48c33ccbc63773
tanyaw <twanwatanakool@gmail.com>
2017-11-03 20:29:01 -0700

    Create Event page.

--- src/main/resources/static/js/PairedController.js
+++ src/main/resources/static/js/PairedController.js
@@ -3,3 +3,8 @@ app.controller('pairedController', function($scope) {
     $scope.firstName = "bill";
     $scope.lastName = "Doe";
 });
+
+app.controller('pairedEventController', function($scope) {
+    $scope.event = "Dia de Los Muertos";
+});
+
