32ba6806247d50cdc62fbca9d1ca2020e1f2d1d0
Anuja Joshi <anuja.j20@gmail.com>
2017-11-14 23:46:22 -0800

    Home page renders everything in angular, just need to format

--- src/main/resources/static/js/PairedController.js
+++ src/main/resources/static/js/PairedController.js
@@ -1,7 +1,25 @@
 var app = angular.module('pairedApp', []);
 app.controller('pairedController', function($scope) {
-     $scope.imageSources = 'images/bric.jpg';
-     
+  
+   $scope.carouselImages = [
+  {source: "/images/bric.jpg", title:"bric"} ,
+  {source:  "/images/calpoly.jpg", title: "cal poly"}  ];
+  
+   $scope.displayEventImages = [
+  {source: "/images/bric.jpg", title:"bric1"} ,
+  {source:  "/images/calpoly.jpg", title: "cal poly1"} ,
+  {source: "/images/bric.jpg", title:"bric2"} ,
+  {source:  "/images/calpoly.jpg", title: "cal poly2"}, 
+   {source: "/images/bric.jpg", title:"bric3"} ,
+  {source:  "/images/calpoly.jpg", title: "cal poly3"} ,
+  {source: "/images/bric.jpg", title:"bric4"} ,
+  {source:  "/images/calpoly.jpg", title: "cal poly4"},
+   {source: "/images/bric.jpg", title:"bric5"} ,
+  {source:  "/images/calpoly.jpg", title: "cal poly5"} ,
+  {source: "/images/bric.jpg", title:"bric5"} ,
+  {source:  "/images/calpoly.jpg", title: "cal poly5"} ];
+  
+   
 });
 
 app.controller('pairedEventController', function($scope) {
