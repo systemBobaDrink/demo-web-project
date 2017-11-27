efa5fc8f94ec4b6e0e81590a1221ad5826bf6839
LeeKann <ekannampuzha@cpp.edu>
2017-11-27 13:14:56 -0800

    Unique filtering of events

--- src/main/resources/static/js/PairedController.js
+++ src/main/resources/static/js/PairedController.js
@@ -19,6 +19,44 @@ app.controller('pairedController', function($scope) {
 
 });
 
+app.filter('unique', function () {
+
+  return function (items, filterOn) {
+
+    if (filterOn === false) {
+      return items;
+    }
+
+    if ((filterOn || angular.isUndefined(filterOn)) && angular.isArray(items)) {
+      var hashCheck = {}, newItems = [];
+
+      var extractValueToCompare = function (item) {
+        if (angular.isObject(item) && angular.isString(filterOn)) {
+          return item[filterOn];
+        } else {
+          return item;
+        }
+      };
+
+      angular.forEach(items, function (item) {
+        var valueToCheck, isDuplicate = false;
+
+        for (var i = 0; i < newItems.length; i++) {
+          if (angular.equals(extractValueToCompare(newItems[i]), extractValueToCompare(item))) {
+            isDuplicate = true;
+            break;
+          }
+        }
+        if (!isDuplicate) {
+          newItems.push(item);
+        }
+
+      });
+      items = newItems;
+    }
+    return items;
+  };
+});
 app.controller('pairedEventController', function($scope, $http) {
 	$scope.userID = "";
 	$scope.getAllEvents = function(){
