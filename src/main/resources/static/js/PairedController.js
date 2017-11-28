a10864f7700cc5ce4870a02006ab174fee4aa311
tanyaw <twanwatanakool@gmail.com>
2017-11-27 17:18:33 -0800

    Updates to CreateEvent form. Updated category dropdown.

--- src/main/resources/static/js/PairedController.js
+++ src/main/resources/static/js/PairedController.js
@@ -83,7 +83,7 @@ app.controller('pairedEventController', function($scope, $http) {
 
 app.controller('pairedCreateEventController', function($scope , $http) {
 	$scope.eventName = "Name of Event";
-	$scope.categories = ["Dance", "Gym and Fitness", "Music", "Professional Development", "Outdoor Activities", "TV Shows", "Study Groups", "Video Games"];	
+	$scope.categories = ["Art", "Fitness", "Music", "Professional Development", "Outdoor Activities", "Rock Climbing", "Study Groups", "Tennis", "TV Shows", "Video Games"];	
 	//3. attach originalStudent model object
 	$scope.originalEvent = {
 			userEmail: '',
